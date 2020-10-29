f34ce7012ca8c6083982ca44231774326a71b101
Aleks Berland <aleksb@yowzaanimation.com>
2020-10-29 17:49:26 -0400

    Disable palettes to publish by default

--- pype/plugins/harmony/publish/collect_palettes.py
+++ pype/plugins/harmony/publish/collect_palettes.py
@@ -30,6 +30,9 @@ class CollectPalettes(pyblish.api.ContextPlugin):
                 "asset": os.environ["AVALON_ASSET"],
                 "subset": "{}{}".format("palette", name)
             })
+
+            instance.data["publish"] = False
+
             self.log.info(
                 "Created instance:\n" + json.dumps(
                     instance.data, sort_keys=True, indent=4
