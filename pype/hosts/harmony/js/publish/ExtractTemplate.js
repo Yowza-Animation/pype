3e65c9f7b600ca399da8bdca8c644ffa14e20083
Aleks Berland <aleksb@yowzaanimation.com>
2020-11-12 10:34:23 -0500

    Refactor extract_template logic for Harmony host

--- pype/hosts/harmony/js/publish/ExtractTemplate.js
+++ pype/hosts/harmony/js/publish/ExtractTemplate.js
@@ -20,54 +20,61 @@ var ExtractTemplate = function() {};
 
 ExtractTemplate.prototype.exportTemplate = function(args) {
     $.beginUndo();
-    var doc = $.scene;
-    var backdrops = args[0];
-    var _nodes = args[1].map(function(x){return doc.$node(x)});
-    var filename = args[2];
-    var folder = args[3];
-    var _refNode = doc.$node(_nodes[0])
-    var currentGroup = _refNode.group
-    const templateGroup = currentGroup.addGroup("temp_group", false, false)
+    try {
+        var doc = $.scene;
+        var backdrops = args[0];
+        var _nodes = args[1].map(function (x) {
+            return doc.$node(x)
+        });
+        var filename = args[2];
+        var folder = args[3];
+        var _refNode = doc.$node(_nodes[0])
+        var currentGroup = _refNode.group
+        const templateGroup = currentGroup.addGroup("temp_group", false, false)
 
-    doc.selectedNodes = _nodes;
+        doc.selectedNodes = _nodes;
 
-    Action.perform("copy()", "Node View");
-    doc.selectedNodes = [templateGroup];
-    Action.perform("onActionEnterGroup()", "Node View");
-    Action.perform("paste()", "Node View");
+        Action.perform("copy()", "Node View");
+        doc.selectedNodes = [templateGroup];
+        Action.perform("onActionEnterGroup()", "Node View");
+        Action.perform("paste()", "Node View");
 
-    // We need to determine the offset deltas
-    var _copiedRefNode = doc.$node(
-        templateGroup.path + "/" + _refNode.name);
+        // We need to determine the offset deltas
+        var _copiedRefNode = doc.$node(
+            templateGroup.path + "/" + _refNode.name);
 
-    var deltaX = _refNode.x - _copiedRefNode.x;
-    var deltaY = _refNode.y - _copiedRefNode.y;
+        var deltaX = _refNode.x - _copiedRefNode.x;
+        var deltaY = _refNode.y - _copiedRefNode.y;
 
-    // Recreate backdrops in group...
-    // @TODO: eventually this should be in callback which does this in a batch
-    //  subprocess to the saved tpl in the temp dir.
-    for (var i = 0 ; i < backdrops.length; i++)
-    {
-        var backdropData = backdrops[i]
-        var newData = JSON.parse(JSON.stringify(backdropData));
-        MessageLog.trace(backdropData);
+        // Recreate backdrops in group...
+        // @TODO: eventually this should be in callback which does this in a batch
+        //  subprocess to the saved tpl in the temp dir.
+        for (var i = 0; i < backdrops.length; i++) {
+            var backdropData = backdrops[i]
+            var newData = JSON.parse(JSON.stringify(backdropData));
+            MessageLog.trace(backdropData);
 
-        // Now fix the backdrop pos with the delta offsets
-        newData["position"]["x"] = backdropData["position"]["x"] + deltaX;
-		newData["position"]["y"] = backdropData["position"]["y"] + deltaY;
-        Backdrop.addBackdrop(templateGroup, newData);
+            // Now fix the backdrop pos with the delta offsets
+            newData["position"]["x"] = backdropData["position"]["x"] + deltaX;
+            newData["position"]["y"] = backdropData["position"]["y"] + deltaY;
+            Backdrop.addBackdrop(templateGroup, newData);
+        }
+
+        Action.perform("selectAll()", "Node View");
+        doc.selectedNodes = templateGroup.nodes
+        copyPaste.createTemplateFromSelection(filename, folder);
+        // Unfocus the group in Node view, delete all nodes and backdrops
+        // created during the process.
+        Action.perform("onActionUpToParent()", "Node View");
+        node.deleteNode(templateGroup, true, true);
     }
-
-    Action.perform( "selectAll()", "Node View" );
-    doc.selectedNodes = templateGroup.nodes
-    copyPaste.createTemplateFromSelection(filename, folder);
-    // Unfocus the group in Node view, delete all nodes and backdrops
-    // created during the process.
-    Action.perform("onActionUpToParent()", "Node View");
-    node.deleteNode(templateGroup, true, true);
-
+    catch(err) {
+        $.endUndo();
+        MessageLog.trace(err);
+        return false;
+    }
     $.endUndo();
-
+    return true;
 };
 
 
